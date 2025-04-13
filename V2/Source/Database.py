4abe48fbda1314f62c5556868fa75e219f817c7c
Harry <harry3.elson@live.uwe.ac.uk>
2025-04-13 17:27:20 +0100

    movies

--- V2/Source/Database.py
+++ V2/Source/Database.py
@@ -257,3 +257,63 @@ class Database:
             self.close()
             print(f"Error adding customer: {e}")
             return None
+
+def add_film(self, title, description, actors, genre, rating, duration):
+    """Add a new film to the database"""
+    conn = self.connect()
+    cursor = conn.cursor()
+
+    try:
+        cursor.execute(
+            """
+            INSERT INTO Films (Title, Description, Actors, Genre, Rating, Duration)
+            VALUES (?, ?, ?, ?, ?, ?)
+            """,
+            (title, description, actors, genre, rating, duration)
+        )
+        conn.commit()
+        film_id = cursor.lastrowid
+        self.close()
+        return film_id
+    except Exception as e:
+        conn.rollback()
+        self.close()
+        print(f"Error adding film: {e}")
+        return None
+
+def populate_films(self, films):
+    conn = self.connect()
+    cursor = conn.cursor()
+    
+    try:
+        cursor.executemany(
+            """
+            INSERT INTO Films (Title, Description, Actors, Genre, Rating, Duration)
+            VALUES (?, ?, ?, ?, ?, ?)
+            """,
+            films
+            title ="A Minecraft Movie",
+            description = "Four misfits are suddenly pulled through a mysterious portal into a bizarre cubic wonderland that thrives on imagination. To get back home they'll have to master this world while embarking on a quest with an unexpected expert crafter.",
+            actors = "Jack Black, Jason Mamoa, Sebastian Hansen",
+            genre = "Comedy",
+            rating = "PG",
+            duration = 101
+        )
+        conn.commit()
+        self.close()
+        return True, f"{cursor.rowcount} films added successfully."
+    except Exception as e:
+        conn.rollback()
+        self.close()
+        return False, f"Error adding films: {e}"
+
+films_to_add = [
+    ("A Minecraft Movie","Four misfits are suddenly pulled through a mysterious portal into a bizarre cubic wonderland that thrives on imagination. To get back home they'll have to master this world while embarking on a quest with an unexpected expert crafter.", "Jack Black, Jason Mamoa, Sebastian Hansen","Comedy","PG", 101)
+    ("The Amateur", "When a CIA cryptographer discovers that terrorists were behind his fiancée's death in a suspicious plane crash, he receives special training in order to plot his revenge.", "Rami Malek, Rachel Brosnahan, Caitriona Balfe, Julianne Nicholson, Holt McCallany","Spy", "12A", 122)
+    ("DROP", "First dates are nerve-wracking enough. Going on a first date while an unnmaed unseen troll pings you person memes that escalate from annoying to homicidal?", "Meghann Fahy, Brandon Sklenar, Violett Beane, Jacob Robinson, Ed Weeks, Jeffery Self", "Thriller","15", 95)
+    ("Death of a unicorn","Father-Daughter duo, Riley and Elliot, hit a unicorn with their car and bring it to the wilderness retreat of a mega-waealthy pharamaceutical CEO.", "Paul Rudd, Jenna Ortega, Téa Leoni, Will Poulter, Richard E Grant", "Comedy / Horror ", "15", 107)
+    ("Until Dawn", "One year after her sister Melanie mysteriously disappeared, Clover and her freidns head into the remote valley where she vanished in search of answers.", "Ella Rubin, Michael Cimino, Odessa Azion, Ji-young Yoo, Belmont Cameli, Maia Mitchell, Peter Stormare", "Adventure", "15", 103)
+    ("Star Wars: Episode III - Revenge of the sith (20th anniversary)", "Three years into the Clone Wars, the Jedi rescue Palpatine from Count Dooku. As Obi-Wan pursues a new threat, Anakin acts as a double agent...", "Christopher Lee, Natalie Portman, Hayden Christensen, Ian McDiarmid, Frank Oz, Ewan McGregor, Samuel L. Jackson, Anthony Daniels, Kenny Baker", "Sci-Fi", "12A", 140)
+    ("The Last Journey", "Follow Filip Hammar as he takes his dad on a road trip through Europe, down to the Mediterranean, with his best mate Fredrik Wikingsson.", "Filip Hammar, Fredrik Wikingsson, Lars Hammar, Tiina Hammar", "Documentary", "PG", 95)
+    ("A Working Man", "Levon Cade left his profession behind to work construction and be a good dad to his daughter. But when a local girl vanishes, he's asked to return.", "Jason Statham, Jason Flemyng, Merab Ninidze, Maximilian Osinski, Cokey Falkow, Michael Pena, David Harbour, Noemi Gonzalez, Arianna Rivas, Emmett J Scanlan, Eve Mauro", "Action / Thriller", "15" , 116)   
+]
